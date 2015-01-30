aab9e9cce08008551585eee36ef56d2fecc64145
Diane Kaplan <dianekaplan@gmail.com>
2015-01-29 21:00:33 -0500

    updated admin.py to include the other tables for the admin UI

--- legacy_test/outer_mysite/family/admin.py
+++ legacy_test/outer_mysite/family/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from family.models import Families
+from family.models import Families, People, Images, Imageperson, Notes, Specialinfo, Specialinfoperson, Users, Updates
+
 # Register your models here.
 
 admin.site.register(Families)
-#admin.site.register(people)
-#admin.site.register(images)
-#admin.site.register(imagePerson)
-#admin.site.register(Notes)
-#admin.site.register(specialinfo)
-#admin.site.register(specialinfoPerson)
-#admin.site.register(users)
-#admin.site.register(updates)
+admin.site.register(People)
+admin.site.register(Images)
+admin.site.register(Imageperson)
+admin.site.register(Notes)
+admin.site.register(Specialinfo)
+admin.site.register(Specialinfoperson)
+admin.site.register(Users)
+admin.site.register(Updates)
