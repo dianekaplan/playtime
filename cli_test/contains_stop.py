68e765fd407b2287e2f74da97dc85a36519d543f
Diane Kaplan <dianekaplan@gmail.com>
2015-01-31 15:10:26 -0500

    Cleaning up

--- cli_test/contains_stop.py
+++ cli_test/contains_stop.py
@@ -1,19 +1,13 @@
 #contains_stop.py
+#This program evaluates a sequence and returns a Boolean: True if the string contains a Stop codon, False if it does not
 #Diane Kaplan 1/31/2015
-# Bugs: no validation that the file is a Fasta DNA file.
-#       Assumes contents are a, c, g, and t.
 # 
-# Usage: To run on the Fasta file "EcoliK12.fasta", type
-#       % python frequency.py EcoliK12.fasta
-#citations: none this time
-
-#3) Write a Python function that takes a string, and returns a Boolean.  
-#It returns True if the string represents a Stop codon, and false if it does not.  
-
-#Take the string, save the codons, search the codons for a stop, return answer
+# Usage: To run on the Fasta file "sequence.fasta", type
+#       % python contains_stop.py sequence.fasta
+#Citations: I re-used Jeff Parker's syntax for pulling the value from a file and running it on the command line
 
 #Assumptions: 
-#The given sequence will be uppercase DNA
+#The given sequence will be DNA (a, c, g, t, not RNA with U's), and will be a fasta file
 #The given sequence will start from the desired reading frame
 
 #Tested: 
@@ -50,36 +44,26 @@ def containsStop(seq):
         
     return contains_stop        
     
+
 #Now let's call it
 #test_sequence = ''
 #test_sequence = 'TAAGGGGGGGGTAA' 
-
 if (len(sys.argv) != 2):
     print "Usage: python", sys.argv[0], "<filename>"
 else:
-    # Get the file name from the command line
-    fileName = sys.argv[1]
+    fileName = sys.argv[1]  # Get the file name from the command line
+    
+    print fileName   # Echo print 
 
-    # Echo print 
-    print fileName
-
-    # Open the file
-    f = open(fileName, 'r')
-
-    # Read the first line of the file
-    # First line in Fasta Format describes the file
-    test_sequence = f.readline()
+    f = open(fileName, 'r')  # Open the file
+    test_sequence = f.readline() # Read the first line of the Fasta file (describes the file)
 
     print "Saw", test_sequence
 
+    #cleanup
     test_sequence = f.read()
-
-    # Remove any new lines with global search and replace
-    #text = text.replace("\n", "")
-
-    # Convert to Upper Case
-    test_sequence = test_sequence.upper()
-    
+    test_sequence = test_sequence.replace("\n", "") # Remove any new lines with global search and replace
+    test_sequence = test_sequence.upper()  # Convert to Upper Case
 
     print "Sequence" , test_sequence, "returns ", containsStop(test_sequence)
 
