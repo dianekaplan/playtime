83bca8bf99116566fc50ec469c1c62195d483473
Diane Kaplan <dianekaplan@gmail.com>
2015-01-31 19:51:55 -0500

    Added stuff for problem 6

--- cli_test/DK_problem_five.py
+++ cli_test/DK_problem_five.py
@@ -4,37 +4,31 @@
 # 
 # Usage: To run on the Fasta file "sequence.fasta", type
 #       % python DK_problem_five.py sequence.fasta
-#Citations: I re-used syntax from Jeff Parker file for pulling the value from a file and running it on the command line
-#Assumptions: 
-#
-#
-#Tested: 
-#positive: 
-#negative: 
+#Citations: I re-used syntax from Jeff Parker file for using command line to pull the value from a file and clean it up for use
+#Assumptions: none
+#Tested: sequences with none, 1 at the start, 1 in the middle, 1 at the end, and multiple instances
  
 import sys      # Used for Command Line Parameters
  
- 
-
 def get_TATAAT_info(seq):
     position_of_first= -1
     position_of_last= -1  
     TATAAT_count = -1  
-    results = [] #this will have the first position of each instance
+    results = [] #this will have the first position of each instance encountered
  
     seqLen = len(seq)  
     cursor = 0
 
-    #Let's get our list of instances
-    while cursor < seqLen-5: #leave off 
+    #Let's make our list of instances
+    while cursor < seqLen-5: 
         this_snippet = seq[cursor:cursor+6]
-        #print this_snippet
-        #see if it is or not
+
+        #if it matches, save the spot and increment our count
         if this_snippet == 'TATAAT':
-            #TATAAT_count += 1
             results.append(cursor)
         cursor +=1    
-       
+
+    #use the list to populate the info we're looking for
     TATAAT_count = len(results)
     if TATAAT_count > 0: 
         position_of_first = results[0]
@@ -43,9 +37,7 @@ def get_TATAAT_info(seq):
     return (position_of_first,position_of_last,TATAAT_count)   
     
                    
-                             
-    
-# Let's check for a file specified on the command line 
+# Check for a file specified on the command line 
 if (len(sys.argv) != 2):
     print "Usage: python", sys.argv[0], "<filename>"
 else:
@@ -55,7 +47,7 @@ else:
     file_description = f.readline() # Read the first line of the Fasta file (describes the file)
     print "This fasta file is:", file_description
 
-    #cleanup
+    #get our sequence clean
     test_sequence = f.read()
     test_sequence = test_sequence.replace("\n", "") # Remove any new lines with global search and replace
     test_sequence = test_sequence.upper()  # Convert to Upper Case
