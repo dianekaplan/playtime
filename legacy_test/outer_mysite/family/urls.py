8836e5ace296dfbaea7f3107f483066443441af8
Diane Kaplan <dianekaplan@gmail.com>
2015-02-10 14:10:40 -0500

    Added a template for index view and placeholder for family detail

--- legacy_test/outer_mysite/family/urls.py
+++ legacy_test/outer_mysite/family/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from family import views
 
 urlpatterns = patterns('', 
+    # ex: /family/
     url(r'^$', views.index, name='index'), 
+    # ex: /family/5/
+    url(r'^(?P<familyID>\d+)/$', views.detail, name='detail'),   
 )
--- legacy_test/outer_mysite/family/views.py
+++ legacy_test/outer_mysite/family/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from family.models import Family, Person
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the family index!")
+    family_list = Family.objects.order_by('caption')
+    context=  { 'family_list': family_list}
+    return render (request, 'family/index.html', context)
+
+def detail(request, familyID):
+   return HttpResponse("You're looking at family %s." % familyID)
+
