4256ee4b73fe223a61eb09c4a5e85d6525d2aa6d
Diane Kaplan <dianekaplan@gmail.com>
2015-05-16 17:40:43 -0400

    Added person index page and person detail page

--- legacy_test/outer_mysite/family/urls.py
+++ legacy_test/outer_mysite/family/urls.py
@@ -6,5 +6,9 @@ urlpatterns = patterns('',
     # ex: /family/
     url(r'^$', views.index, name='index'), 
     # ex: /family/5/
-    url(r'^(?P<familyID>\d+)/$', views.detail, name='detail'),   
+    url(r'^(?P<familyID>\d+)/$', views.detail, name='detail'), 
+    # ex: /person/
+    url(r'^person/$', views.person_index, name='person_index'), 
+    # ex: /person/5/
+    url(r'^person/(?P<personID>\d+)/$', views.person_detail, name='person_detail'),  
 )
--- legacy_test/outer_mysite/family/views.py
+++ legacy_test/outer_mysite/family/views.py
@@ -17,4 +17,22 @@ def detail(request, familyID):
 
     #return HttpResponse("You're looking at family %s." % familyID)
     return render (request, 'family/family.html', {'family': familyID})
+    #family_list = Family.objects.get(pk=familyID)
+    #context=  { 'family_list': family_list}
+    #return render (request, 'family/family.html', context)
+
+# New stuff 5/16:
+def person_index(request):
+    person_list = Person.objects.order_by('last')
+    context=  { 'person_list': person_list}
+    return render (request, 'family/person_index.html', context)
+
+def person_detail(request, personID):
+    try: 
+        Person.objects.get(pk=personID)
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist")
+
+    #return HttpResponse("You're looking at family %s." % familyID)
+    return render (request, 'family/person.html', {'person': personID})
 
